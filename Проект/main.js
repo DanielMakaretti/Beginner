8326a13a10a7b834fe055b4762043e2b66d55aec
Daniel Makaretti <139051146+DanielMakaretti@users.noreply.github.com>
2023-07-18 20:54:59 +0300

    created a site layout

---
+++ Проект/main.js
@@ -0,0 +1,11 @@
+/* Javascript */
+ 
+$(function () {
+ 
+    $(".rateYo").rateYo({
+     
+    });
+    var normalFill = $("#rateYo").rateYo("option", "normalFill");
+    $("#rateYo").rateYo("option", "normalFill", "#F39C21");
+    
+  });
