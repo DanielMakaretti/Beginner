faeb6f15aa20d22c5393d887d7007aaa4154bf5e
Daniel Makaretti <139051146+DanielMakaretti@users.noreply.github.com>
2023-07-20 13:55:13 +0300

    New section

--- Проект/main.js
+++ Проект/main.js
@@ -1,9 +1,10 @@
 /* Javascript */
  
+console.log('Привет от JavaScript!');
 $(function () {
  
-  var mixer = mixitup('.directions__list');
-    $('directions__filter-btn'), on('click', function (e) {
+ var mixer = mixitup('.directions__list');
+   /* $('directions__filter-btn'), on('click', function (e) {
       $(this).addClass('directions__filter-btn--all')
-    })
-  });
+    })*/
+  })
