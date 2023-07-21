9e79416cfc21abc4b622161809cd0dbb65abedbb
Daniel Makaretti <139051146+DanielMakaretti@users.noreply.github.com>
2023-07-21 12:59:45 +0300

    Сreating a slider

--- Проект/main.js
+++ Проект/main.js
@@ -3,7 +3,12 @@
 $(function () {
  
  var mixer = mixitup('.directions__list');
-   /* $('directions__filter-btn'), on('click', function (e) {
+    $('directions__filter-btn'), on('click', function () {
+      $('.directions__filter-btn').removeCLass('directions__filter-btn--all')
       $(this).addClass('directions__filter-btn--all')
-    })*/
+    })
+
+    $('.team__slider').slick({
+
+    })
   })
