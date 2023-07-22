3c6366397e5b87240e7d0e27b03ea28956816df2
Daniel Makaretti <139051146+DanielMakaretti@users.noreply.github.com>
2023-07-22 22:57:58 +0300

    Slider

--- Проект/main.js
+++ Проект/main.js
@@ -3,12 +3,15 @@
 $(function () {
  
  var mixer = mixitup('.directions__list');
-    $('directions__filter-btn'), on('click', function () {
-      $('.directions__filter-btn').removeCLass('directions__filter-btn--all')
+ 
+    $('directions__filter-btn').on('click', function () {
+      $('.directions__filter-btn').removeCLass('directions__filter-btn--active')
       $(this).addClass('directions__filter-btn--all')
-    })
-
+    });
+  });
     $('.team__slider').slick({
-
+arrows: false,
+slidesToShow: 4,
+Infinite: true,
     })
-  })
+ 
