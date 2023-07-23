d5483fcf7e83d7549ddb9e9de72012aa888e0197
Daniel Makaretti <139051146+DanielMakaretti@users.noreply.github.com>
2023-07-23 16:22:46 +0300

    The end slider sections

--- Проект/main.js
+++ Проект/main.js
@@ -10,9 +10,20 @@ $(function () {
     });
   });
     $('.team__slider').slick({
+      arrows: false,
 slidesToShow: 4,
 Infinite: true,
 draggable: false,
-appendArrows: $('.team__slider-arrows'),
-    })
+waitForAnimate: false,
+});
+    $('.team__slider-prev').on('click', function (e) {
+      e.preventDefault()
+      $('.team__slider').slick('slickPrev')
+    });
+    $('.team__slider-next').on('click', function (e) {
+      e.preventDefault()
+      $('.team__slider').slick('slickNext')
+    });
+   
+   
  
