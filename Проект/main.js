0757f9c1c3c0d2d23dc00a2d7490667751394c59
Daniel Makaretti <139051146+DanielMakaretti@users.noreply.github.com>
2023-07-30 19:54:30 +0300

    New section course program

--- Проект/main.js
+++ Проект/main.js
@@ -42,8 +42,47 @@ $('.testimonials__next').on('click', function (e) {
   $('.testimonials__slider').slick('slickNext')
 });
 
+// $('.program__acc-link').on('click', function (e) {
+// e.preventDefault()
+// $(this).toggleClass('program__acc-link--active')
+// $(this).children('.program__acc-text').slideToggle()
+// });
 
- 
+// $('.program__acc-link').on('click', function (e) {
+//   e.preventDefault()
+//   $('.program__acc-link').removeCLass('program__acc-link--active')
+//   $(this).toggleClass('program__acc-link--active')
+//   $(this).children('.program__acc-text').slideToggle()
+//   if ($('.program__acc-link').hasClass('program__acc-link--active')) {
+//   $('.program__acc-link').removeCLassClass('program__acc-link--active')
+//   }
+//   });
+   
+  // $('.program__acc-link').on('click', function (e) {
+  //   e.preventDefault()
+  //   if ($(this).hasClass('program__acc-link--active')) {
+  //     $(this).removeCLass('program__acc-link--active')
+  //     $(this).children('.program__acc-text').slideUp()
+  //   } else {
+  //     $('.program__acc-link').removeCLass('program__acc-link--active')
+  //     $('.program__acc-text').slideUp()
+  //     $(this).addClass('program__acc-link--active')
+  //     $(this).children('.program__acc-text').slideDown()
+  //   }
+  // });
+
+  $('.program__acc-link').on('click', function(e){
+    e.preventDefault()
+    if ($(this).hasClass('program__acc-link--active')) {
+        $(this).removeClass('program__acc-link--active')
+        $(this).children('.program__acc-text').slideUp()
+        stopImmediatePropagation()
+    }   
+    $('.program__acc-link').removeClass('program__acc-link--active')
+    $('.program__acc-text').slideUp()
+    $(this).toggleClass('program__acc-link--active')
+    $(this).children('.program__acc-text').slideDown()
+})
 
 
    
